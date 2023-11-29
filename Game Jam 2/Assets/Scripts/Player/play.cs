444fad09863060abad1e39e72cc6a91f8aff0dfa
GUFFYCLUCK <19803@students.isb.ac.th>
2023-11-29 10:40:53 +0700

    FIRE!!

--- Game Jam 2/Assets/Scripts/Player/play.cs
+++ Game Jam 2/Assets/Scripts/Player/play.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class play : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform firingPoint;
-    [Range(0.1f, 1.0f)] // Corrected the range for fireRate
+    [Range(0.1f, 2f)] 
     [SerializeField] private float fireRate = 0.5f;
-    private float nextFireTime = 0f; // Added a variable to track the next allowed fire time
+
     private Rigidbody2D rb;
     private float mx;
     private float my;
     private Vector2 mp;
+
+    private float fireTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -29,14 +30,18 @@ public class play : MonoBehaviour
         my = Input.GetAxisRaw("Vertical");
         mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float angle = Mathf.Atan2(mp.y - transform.position.y, mp.x - transform.position.x) * Mathf.Rad2Deg - 90f;
-        transform.localRotation = Quaternion.Euler(0, 0, angle);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
         rb.velocity = new Vector2(mx, my).normalized * speed;
 
         // Check if the current time is greater than the next allowed fire time
-        if (Input.GetKeyDown(KeyCode.E) && Time.time > nextFireTime)
+        if (Input.GetKeyDown(KeyCode.E) && fireTimer <= 0f)
         {
             Shoot();
-            nextFireTime = Time.time + 1 / fireRate; // Update the next allowed fire time
+            fireTimer = fireRate;
+        }
+        else
+        {
+            fireTimer -= Time.deltaTime; // Corrected the decrement of fireTimer
         }
     }
 
